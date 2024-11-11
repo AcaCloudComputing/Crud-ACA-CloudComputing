fed19bb758146a059b103e8829302b993d9357c8
AcaCloudComputing <acacloudcptng@gmail.com>
2024-11-11 13:21:20 -0500

    Change files

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyCUUPppqpcHPDwkUuE0Lk0kXdnhVK0FrFE",
+        authDomain: "crud-aca-cloudcomputing.firebaseapp.com",
+        projectId: "crud-aca-cloudcomputing",
+        storageBucket: "crud-aca-cloudcomputing.firebasestorage.app",
+        messagingSenderId: "49267842645",
+        appId: "1:49267842645:web:4534892f4b054c5ff67edc"
+      }
+};
